2482b7a434a36946b4ea6a31441acd80aca59ebb
zengxp@home-win10 <xiaoping_zeng@foxmail.com>
2016-10-18 22:09:51 +0800

    update

--- curl.js
+++ curl.js
@@ -1,4 +1,5 @@
 var request = require('superagent'),
+    fs = require('fs'),
     connector = {},
     baseURL = 'http://localhost:8080/Server/rest/',
     Server_Session;
@@ -18,7 +19,65 @@ connector.test = function() {
         });
 }
 
-connector.test();
+//connector.test();
+
+var counter = 1;
+var maxTry = 500;
+
+connector.baidu = function() {
+
+  var url = 'https://passport.baidu.com/center';
+   
+   var _cookieStr = 'BAIDUID=4FFE2A8BB9E79D74FEFD070D3D99900D:FG=1;BDUSS=VpQXhWdkJQck9DSEozS2VraGxYTDBVU09RVDNEYkdNWTk0YTV0QnJmRWxjU2hZSVFBQUFBJCQAAAAAAAAAAAEAAAAYWc4jemVuZ3hwMDYwNQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACXkAFgl5ABYVl;HOSUPPORT=1;Hm_lpvt_90056b3f84f90da57dc0f40150f005d5=1476453515;Hm_lvt_90056b3f84f90da57dc0f40150f005d5=1476453507,1476453515;PTOKEN=da84e6fb6755d8e0a08983fad1a013e1;STOKEN=bb0ba3d90bfbf3dc6e4605f9aa2b5ff0af16eb1e5ab08070cc8ce1e28226f490; ';
+    request
+      .get(url)
+      .set('Cookie', _cookieStr)
+      .end(function(err, resp) {
+          if(!err && resp.ok){
+            var _html = resp.text;
+            console.log(resp.ok);
+            var _subStr = '<a href="http://www.baidu.com/p/setting/profile/basic" target="_blank">修改资料</a>';   
+            if(_html.includes(_subStr)){
+                var msg = `${counter} is ok\r\n`;
+                console.log(msg);
+                writeLog(msg, 'success');
+            } else {
+                fs.writeFileSync('./tmp/' + counter + '.txt', _html);
+                let msg = `${counter} login status error\r\n`;
+                writeLog(msg, 'error');
+                console.log(msg);
+            }
+          } else {
+              console.log(err, 'read-error: ' , counter);
+          }
+      });
+}
+
+var timer = null;
+
+connector.baidu();
+
+timer = setInterval(function(){
+
+  counter ++;
+  if(counter > maxTry){
+    timer = null;
+    clearInterval(timer);
+    return false;
+  }
+  connector.baidu();
+}, 120 * 1000);
+
+
+function writeLog(msg, type = 'error'){
+  let _time = require('moment')().format("YYYY-MM-DD HH:mm:ss");
+  require('fs').appendFile('./tmp/' + type + '.log', _time + '  ' + msg, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    // console.log('The "data to append" was appended to file!');
+  });
+}
 
 connector.EstablishSession = function() {
     var url = baseURL + 'session/establish';
